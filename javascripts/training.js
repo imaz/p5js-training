cc832e48a37e8209b6279a122922d501fcf90110
IMAZU Asami <neko.daisuki.070109@gmail.com>
2015-09-04 00:11:06 +0900

    Draw mini ellipse

--- javascripts/training.js
+++ javascripts/training.js
@@ -1,1 +1,10 @@
-alert('hi');
+function setup() {
+  createCanvas(800, 400);
+  background(0, 0, 0);
+}
+
+function draw() {
+  stroke(100, 100, 100);
+  fill(80, 90, 100, 30)
+  ellipse(mouseX, mouseY, 10, 10);
+}
